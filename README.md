21c5f13ca49f21fba39024532e34bb3a6630af74
g7tianyi <g7tianyi@gmail.com>
2022-03-26 00:24:27 +0800

    [feat] auto tag scripts

--- README.md
+++ README.md
@@ -2,12 +2,8 @@
 
 All persistence models in Mongo.
 
-> Please create git tags when push changes.
->
-> https://github.com/Overealityio/overeality-server-model/tags
-> 
+> Please create git tags when push changes, please refer to [release.sh](./release.sh).
 
 ~~~bash
-git tag -a v0.0.5 -m "commit message here"
-git push -u origin v0.0.5
+./release.sh
 ~~~
---
+++ release.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+pushd $(dirname $0) >/dev/null
+SCRIPTPATH=$(pwd -P)
+popd >/dev/null
+SCRIPTFILE=$(basename $0)
+
+function log() {
+  echo "================================================================================"
+  echo "$(date +'%Y-%m-%d %H:%M:%S%z') [INFO] - $@"
+  echo ""
+}
+
+VERSION=$(cat ${SCRIPTPATH}/version.txt)
+
+MAJOR=$(echo $VERSION | awk -F'.' '{ print $1 }')
+MINOR=$(echo $VERSION | awk -F'.' '{ print $2 }')
+PATCH=$(echo $VERSION | awk -F'.' '{ print $3 }')
+
+PATCH=$((${PATCH} + 1))
+if [[ ${PATCH} == 10 ]]; then
+  PATCH=0
+  MINOR=$((${MINOR} + 1))
+fi
+if [[ ${MINOR} == 10 ]]; then
+  MINOR=0
+  MAJOR=$((${MAJOR} + 1))
+fi
+
+VERSION="${MAJOR}.${MINOR}.${PATCH}"
+echo ${VERSION} >${SCRIPTPATH}/version.txt
+
+# git add .
+# git commit -m "prepare to publish v${VERSION}"
+log "publish overeality-server-model v${VERSION}"
+git tag -a "v${VERSION}" -m "publish overeality-server-model v${VERSION}"
+git push -u origin v${VERSION}
